5a07b4148157f8fda2dabcc83aa4e3dbc9f15129
Ashley <tanaka.mush@gmail.com>
2024-10-07 15:29:29 +0100

    fixed security vulnerability

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_APP_KEY')
-    SQLALCHEMY_DATABASE_URI =  'sqlite:///Agency.db'
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False  # Set to False in production
     EMAIL_KEY = os.environ.get('EMAIL_KEY')

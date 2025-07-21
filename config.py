7e8b0a94f00d4b5fe7908a186917db4bbadd2b52
Ashley <tanaka.mush@gmail.com>
2025-07-21 22:16:47 +0100

    Fixed linked in information which was redirecting to the wrong url

--- config.py
+++ config.py
@@ -3,10 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_APP_KEY', 'fallback-secret-key')
-
-    # If DATABASE_URI is not set in environment, default to local SQLite
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///BlogDataBase.db"
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///BlogDataBase.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.environ.get('FLASK_DEBUG', False)  # Use env var to toggle debug mode
     EMAIL_KEY = os.environ.get('EMAIL_KEY')

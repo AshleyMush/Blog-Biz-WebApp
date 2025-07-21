5e74667820c23e062adf12a975b766db281f7836
Ashley <tanaka.mush@gmail.com>
2025-07-21 20:51:32 +0100

    Added SEO meta data for socials and new favicon. Added add space on blog index.

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-from flask import Flask
 import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_APP_KEY')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URI", f"sqlite:///BlogDataBase"
-    )  # Default to SQLite if DATABASE_URI is not set
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_APP_KEY', 'fallback-secret-key')
+
+    # If DATABASE_URI is not set in environment, default to local SQLite
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///BlogDataBase.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = False  # Set to False in production
+    DEBUG = os.environ.get('FLASK_DEBUG', False)  # Use env var to toggle debug mode
     EMAIL_KEY = os.environ.get('EMAIL_KEY')
     PASSWORD_KEY = os.environ.get('PASSWORD_KEY')
